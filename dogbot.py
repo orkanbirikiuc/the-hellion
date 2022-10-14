8cb65e8e4137950882f187078b61c8c415d6a053
orkanb2u <orkanbirikiuc@gmail.com>
2022-10-14 05:07:45 +0300

    Modified bop

--- dogbot.py
+++ dogbot.py
@@ -16,24 +16,10 @@ def start(update, context):
     context.bot.send_message(chat_id=chat_id, text="Beep boop! \n/bop for dog pics \n/mew for cats \n/meow for cat facts")
 
 
-# bop for dog
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
-
-def get_image_url():
-    allowed_extension = ['jpg', 'jpeg', 'png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$", url).group(1).lower()
-    return url
-
 def bop(update, context):
     url = get_image_url()
     chat_id = update.message.chat_id
-    context.bot.send_photo(chat_id=chat_id, photo=url)
+    context.bot.message(chat_id=chat_id, text="selam")
 
 
 # mew for cat
